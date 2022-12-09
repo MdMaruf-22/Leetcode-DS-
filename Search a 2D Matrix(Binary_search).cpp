8502c5dcb2ed82bab8db19a7e0c1e9fece219834
Mohammed Maruf Islam <64244354+MdMaruf-22@users.noreply.github.com>
2022-12-09 14:18:49 +0600

    Update Search a 2D Matrix(Binary_search).cpp

--- Search a 2D Matrix(Binary_search).cpp
+++ Search a 2D Matrix(Binary_search).cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row=0;
-        int col=matrix[0].size()-1;
-        while(row<matrix.size() && col>=0){
-            if(matrix[row][col]==target) return true;
-            else if(matrix[row][col]<target) row++;
-            else col--;
+        int row=matrix.size();int col=matrix[0].size();
+        int low=0,high=row*col-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int val=matrix[mid/col][mid%col];
+            if(val==target) return true;
+            else if(val>target) high=mid-1;
+            else low=mid+1;
         }
         return false;
     }
