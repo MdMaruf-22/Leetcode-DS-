4733e2bd220fca3c42e10bf586fc6009c117d86f
Mohammed Maruf Islam <64244354+MdMaruf-22@users.noreply.github.com>
2022-12-21 19:20:18 +0600

    Update Valid Anagram.cpp

--- Valid Anagram.cpp
+++ Valid Anagram.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int> vc(26,0);
-        for(auto i:s) vc[i-'a']++;
-        for(auto i:t){
-            if(vc[i-'a']==0) return false;
-            vc[i-'a']--;
+        int chk[255]={0};
+        int sz1=s.size(),sz2=t.size();
+        if(sz1!=sz2) return false;
+        for(int i=0;i<sz1;i++){
+            chk[s[i]]+=1;
         }
-        for(auto x:vc){
-            if(x>=1) return false;
+        for(int i=0;i<sz2;i++){
+            chk[t[i]]-=1;
+        }
+        for(int i=0;i<255;i++){
+            if(chk[i]!=0) return false;
         }
         return true;
     }
